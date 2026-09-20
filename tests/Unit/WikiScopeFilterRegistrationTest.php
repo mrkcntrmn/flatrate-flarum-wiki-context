49eaf388866d8b9a6082b8b9223c64d23ca176e9
Mark Counterman <ilovepoker@gmail.com>
2026-09-20 13:41:47 -0500

    test(wiki001e): inject settings into wiki scope filter test

--- tests/Unit/WikiScopeFilterRegistrationTest.php
+++ tests/Unit/WikiScopeFilterRegistrationTest.php
@@ -3,13 +3,17 @@
 namespace FlatRate\WikiContext\Tests\Unit;
 
 use FlatRate\WikiContext\Filter\WikiScopeFilter;
+use FlatRate\WikiContext\Projection\SettingsReader;
+use Flarum\Settings\SettingsRepositoryInterface;
 use PHPUnit\Framework\TestCase;
 
 final class WikiScopeFilterRegistrationTest extends TestCase
 {
     public function test_filter_key(): void
     {
-        $filter = new WikiScopeFilter();
+        $settings = $this->createMock(SettingsRepositoryInterface::class);
+        $filter = new WikiScopeFilter(new SettingsReader($settings));
+
         $this->assertSame('wiki-scope', $filter->getFilterKey());
         echo "WIKI_SCOPE_FILTER_REGISTRATION=PASS\n";
     }
